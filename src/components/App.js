b68851c7224ea7ecf5c3d2df9d8302fb4e41e968
alialpersakar <aalpersakar@gmail.com>
2020-02-19 15:16:43 +0300

    Slider-Responsiveness-HamburgerMenu

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import MainLayout from "../layouts/MainLayout";
 import Login from "../components/Login";
 import '../css/Main.css';
+import {Button} from "semantic-ui-react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 
 class App extends Component {
     constructor() {
@@ -11,12 +18,28 @@ class App extends Component {
 
     render() {
         return (
-            <div className="main">
-                <MainLayout />
-                {/* <Login /> */}
-            </div>
+            <Router>
+                <div>
+                    <Switch>
+                        <Route path="/" exact component={Home}/>
+                        <Route path="/homepage" component={MainLayout} />
+                        <Route path="/login" component={Login} />
+                    </Switch>
+                    {/* <MainLayout /> */}
+                    {/* <Login /> */}
+                </div>
+            </Router>
         )
     }
 }
+
+const Home = () => (
+    <div className="home">
+        <h1>Home Page</h1>
+        <Link to="/login"><Button primary size='huge'>Login</Button></Link>
+        <Link to="/homepage"><Button primary size='huge'>Home Page</Button></Link>
+
+    </div>
+);
 
 export default App;
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
-import Navbar from '../organisms/Navbar';
-import'../css/MainLayout.css';
-import Menu from '../organisms/Menu';
+// import Navbar from '../organisms/Navbar';
+import '../css/MainLayout.css';
+// import Menu from '../organisms/Menu';
+import Menu from '../organisms/newMenu';
+import bilsportlogo from '../images/fotos/bilsportlogo.png';
+import Bbuilding from '../images/fotos/Bbuilding.jpg'
+import AwesomeSlider from 'react-awesome-slider';
+import sportcenter from '../images/fotos/sportcenter.jpg'
+import 'react-awesome-slider/dist/styles.css';
+
+
+
+const slider = (
+    <AwesomeSlider>
+        <div data-src={bilsportlogo} />
+        <div data-src={Bbuilding} />
+        <div data-src={sportcenter} />
+    </AwesomeSlider>
+);
+
 class MainLayout extends Component {
     constructor() {
         super();
@@ -9,18 +26,35 @@ class MainLayout extends Component {
     }
 
     render() {
+        let img = React.createElement("img", { href: '../images/fotos/bilsport.png' });
+
         return (
             <div>
-                <div className="menu">
-                    <Menu />
+                <Menu />
+                <div className="content">
+                    {/* <div className="img">
+                        <img src={bilsportlogo}/>
+                    </div> */}
+                    <div className="img">
+                        {slider}
+                    </div>
+                    <div className="announcement">
+                        Beden Eğitim ve Spor Merkezi  
+                        Beden Eğitimi ve Spor Merkezi’nin misyonu, Bilkent Üniversitesi öğrencilerine sağlıklı yaşam alışkanlıkları kazandıracak ortamı hazırlamak, kampüs yaşantısını zenginleştirmek ve kaliteli beden eğitimi, spor ve kondisyon programları düzenleyerek öğrencilere hayat boyu kullanabilecekleri spor becerileri kazandırmaktır.
+                        2020 Bahar yarıyılı kayıtları, Beden Eğitimi ve Spor Merkezi’nde, 3-9 Şubat 2020 tarihleri arasında yapılacaktır.
+                        Kurslarımız 10 Şubat 2020’de başlayacak, 3 Mayıs 2020’de sona erecektir.
+                    </div>
+                </div>
+                {/* <div className="menu">
+                    <Menu title='Title'/>
                 </div>
                 <div className="navBar">
                     <Navbar />
                 </div>
                 <div className="content">
                     Contents
-                </div>
-              
+                </div> */}
+
             </div>
         )
     }
